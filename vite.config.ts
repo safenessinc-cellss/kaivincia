c887c42f95bdf0f9010f96a182554e720372d4e7
safenessinc-cellss <safeness.inc@gmail.com>
2026-05-12 11:03:47 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [react(), tailwindcss()],
     define: {
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.VITE_GEMINI_API_KEY || ''),
-      'import.meta.env.VITE_GEMINI_API_KEY': JSON.stringify(AIzaSyBlQ-7QHws8kal0SXvXvBK-2z_jgKnYNps),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
     },
     server: {
       port: 3000,
